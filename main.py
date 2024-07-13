fedf73382597959899318241f5f20d72f177d6ff
Milya <m.shamsutdinova@innopolis.university>
2024-07-13 19:14:01 +0300

    added imagepreprocessing

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from code.OIv7Loader import OIv7Loader
+from code.DetectionCut import DetectionCut
+from IPython.display import display
 
-dataloader = OIv7Loader(["Flower"], 50)
-dataset = dataloader.load_data()
+# dataloader = OIv7Loader(["Flower"], 50)
+# dataset = dataloader.load_data()
 
-print(dataset)
+# print(dataset)
+
+processing = DetectionCut()
+cropped_images = processing.process_dataset()
+
+print(cropped_images[0])
+display(cropped_images[0])
