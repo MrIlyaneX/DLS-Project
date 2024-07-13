2b3148f93be6da9762da9a1cebadf8c64653eccd
Milya <m.shamsutdinova@innopolis.university>
2024-07-13 18:22:01 +0300

    data added, imagepreprocessing class added

--- code/Base/ImageProcessingBase.py
+++ code/Base/ImageProcessingBase.py
@@ -1,9 +1,15 @@
-from typing import Any
+import fiftyone as fo
+import fiftyone.zoo as foz
+from PIL import Image
+from typing import List, Tuple, Any
 
 
 class ImageProcessingBase:
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         pass
 
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        raise NotImplemented
+    def process_dataset(self, data: fo.core.dataset.Dataset) -> List[Image.Image]:
+        raise NotImplementedError
+
+    def process_image(self, image: Image.Image) -> List[Image.Image]:
+        raise NotImplementedError
