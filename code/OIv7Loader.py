cdd92025169209a59f3ae76336b78c4fe5a7a78a
Milya <m.shamsutdinova@innopolis.university>
2024-07-13 17:35:27 +0300

    dataloader added

---
+++ code/OIv7Loader.py
@@ -0,0 +1,27 @@
+from typing import Any, List
+import fiftyone as fo
+import fiftyone.zoo as foz
+from PIL import Image
+from Base.DataLoader import DataLoader
+
+
+class OIv7Loader(DataLoader):
+    num_images: int
+    classes: List[str]
+    dataset: fo.core.dataset.Dataset
+
+    def __init__(self, classes: List[str], num_images: int, *args: Any, **kwargs: Any) -> None:
+        super().__init__(classes, num_images, *args, **kwargs)
+        self.dataset = None
+
+    def load_data(self) -> List:
+        self.dataset = foz.load_zoo_dataset(
+            "open-images-v7",
+            split="train",
+            label_types=["detections"],
+            classes=self.classes,
+            max_samples=self.num_images,
+            # shuffle=True
+        )
+
+        return self.dataset
